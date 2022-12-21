507c5413939f3702e53935117be5a11a5038ebe1
Giovanni Poblete <giovannipoblete55@gmail.com>
2022-12-21 01:19:07 -0300

    sd

---
+++ src/main/java/cl/subox/ms/aud/domain/entities/role/Role.java
@@ -0,0 +1,22 @@
+package cl.subox.ms.aud.domain.entities.role;
+
+import cl.subox.ms.aud.domain.entities.role.enums.RoleName;
+import cl.subox.ms.aud.domain.entities.role.enums.RoleRank;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private RoleName name;
+
+    @Column(name = "role_rank")
+    private RoleRank rank;
+}
